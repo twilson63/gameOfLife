31f11aded60883a8ba8a34f167cc76ef3c1a3ce0
brent.cobb <brent.tm.cobb@gmail.com>
2013-09-04 11:03:07 -0400

    added some interaction
    start, stop and randomize buttons added.  I'm sure there is a better
    way to implement "stop", but I thought this method was kind of…
    eloquent.

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,115 +1,99 @@
+var dimension = 15;
+var chanceOfLiveCell = 0.5;
+var table;
+var cell;
 
-function GameOfLife () {
- 
-    this.init = function (turns,width,height) {
-		this.board = new Array(height);
-		for (var x = 0; x < height; x++) {
-			this.board[x] = new Array(width);
-			for (var y = 0; y < width; y++) {
-				this.board[x][y] = Math.round(Math.random());
-			}
+$( document ).ready(function() {
+    table = $("#main");
+	initialiseGame();
+	cells = table.find("td");
+	//placeRandomCells();
+	//playGame();
+	$("#randomize").click(placeRandomCells);
+	$("#start").click(playGame);
+	$("#stop").click(function() { location.reload(); });
+	
+});
+
+function initialiseGame() {
+	var trHtml = [];
+	for (var y = 0; y < dimension; ++y) {
+	trHtml.push("<tr>");
+		for (var x = 0; x < dimension; ++x) {
+			trHtml.push("<td>&nbsp;</td>");
 		}
-		this.turns = turns;
-	};
- 
-	this.nextGen = function() {
-		this.boardNext = new Array(this.board.length);
-		for (var i = 0; i < this.board.length; i++) {
-			this.boardNext[i] = new Array(this.board[i].length);
-		}
-		for (var x = 0; x < this.board.length; x++) {
-			for (var y = 0; y < this.board[x].length; y++) {
-				var n = 0;
-				for (var dx = -1; dx <= 1; dx++) {
-					for (var dy = -1; dy <= 1; dy++) {
-						if ( dx === 0 && dy === 0){}
-						else if (typeof this.board[x+dx] !== 'undefined' && 
-                            typeof this.board[x+dx][y+dy] !== 'undefined' && 
-                            this.board[x+dx][y+dy]) {
-							n++;
-						}
-					}	
-				}
-				var c = this.board[x][y];
-				switch (n) {
-					case 0:
-					case 1:
-						c = 0;
-						break;
-					case 2:
-						break; 
-					case 3:
-						c = 1;
-						break;
-					default:
-						c = 0;
-				}
-				this.boardNext[x][y] = c;
-			}
-		}
-		this.board = this.boardNext.slice();
-	};
- 
-	this.toScreen = function() {
-		for (var x = 0; x < this.board.length; x++) {
-			var l = "";
-			for (var y = 0; y < this.board[x].length; y++) {
-				if (this.board[x][y])
-					l += "X ";
-				else
-					l += "  ";
-			}
-			console.log(l);
-		}
-	};
- 
-	this.start = function() {
-		for (var t = 0; t < this.turns; t++) {
-			console.log("\nTurn "+(t+1));
-			this.toScreen();
-			this.nextGen();
-		}
-	};
- 
+		trHtml.push("</tr>");
+	}
+	trHtml = trHtml.join("");
+	table.append($(trHtml));
 }
- 
- 
-var game = new GameOfLife();
-/* 
-console.log("Oscillator Blinker 3x3 three turns: \n");
-game.init(3);
-game.board = [
-	[0,0,0],
-	[1,1,1],
-	[0,0,0]];
-game.start();
- 
-console.log("Spaceship Glider 10x6 five turns: \n");
-game.init(5);
-game.board = [
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,1,0,0,0,0,0,0,0],
-	[0,0,0,1,0,0,0,0,0,0],
-	[0,1,1,1,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0]];
-game.start();
- 
-console.log("Random 5x10 five turns: \n");
-game.init(5,5,10);
-game.start();
-*/
-console.log("Random 20x20 five turns: \n");
-game.init(5,20,20);
-game.start();
 
-console.log("Oscillator Beacon 6x6 three turns: \n");
-game.init(3);
-game.board = [
-        [0,0,0,0,0,0],
-        [0,1,1,0,0,0],
-        [0,1,0,0,0,0],
-        [0,0,0,0,1,0],
-        [0,0,0,1,1,0],
-        [0,0,0,0,0,0]];
-game.start();
+function placeRandomCells() {
+	for (var y = 0; y < dimension; ++y) {
+		for (var x = 0; x < dimension; ++x) {
+			var cell = getCell(x,y);
+			if (Math.random() > chanceOfLiveCell) {
+				cell.addClass("alive");
+			} else {
+				cell.removeClass("alive");
+			}
+		}
+	}
+}
+
+function playGame() {
+	prepareNextGeneration();
+	renderNextGeneration();
+	setTimeout('playGame()', 200);
+}
+
+function prepareNextGeneration() {
+	for (var y = 0; y < dimension; ++y) {
+		for (var x = 0; x < dimension; ++x) {
+			var cell = getCell(x,y);
+			var neighbors = getLiveNeighborCount(x,y);
+			cell.attr("isalive", "false");
+			if (isCellAlive(x,y)) {
+				if (neighbors === 2 || neighbors === 3) {
+					cell.attr("isalive", "true");
+				}
+			} else if (neighbors === 3) {
+				cell.attr("isalive", "true");
+			}
+		}
+	}
+}
+
+function renderNextGeneration() {
+	cells.each(function () {
+		var cell = $(this);
+		cell.removeClass("alive");
+      if (cell.attr("isalive") === "true") { cell.addClass("alive"); }
+		cell.removeAttr("isalive");
+	});
+}
+
+function getLiveNeighborCount(x,y) {
+	var count = 0;
+	if (isCellAlive(x-1, y-1)) {count++;}
+	if (isCellAlive(x, y-1)) {count++;}
+	if (isCellAlive(x+1, y-1)) {count++;}
+	if (isCellAlive(x-1, y)) {count++;}
+	if (isCellAlive(x+1, y)) {count++;}
+	if (isCellAlive(x-1, y+1)) {count++;}
+	if (isCellAlive(x, y+1)) {count++;}
+	if (isCellAlive(x+1, y+1)) {count++;}
+	return count;
+}
+
+function isCellAlive(x,y) {
+	return getCell(x,y).attr("class") === "alive";
+}
+
+function getCell(x,y) {
+	if (x >= dimension) {x = 0;}
+	if (y >= dimension) {y = 0;}
+	if (x < 0) {x = dimension - 1;}
+	if (y < 0) {y = dimension - 1;}
+	return $(cells[y * dimension + x]);
+}
