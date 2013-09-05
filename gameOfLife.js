63caf4470cabab48dd56d82fc6f48677ffcc3e76
Tom Wilson <tom@jackhq.com>
2013-09-05 00:11:10 -0400

    refactored the html page

--- gameOfLife.js
+++ gameOfLife.js
@@ -5,41 +5,41 @@ var cell;
 
 $( document ).ready(function() {
     table = $("#main");
-	initialiseGame();
-	cells = table.find("td");
-	//placeRandomCells();
-	//playGame();
-	$("#randomize").click(placeRandomCells);
-	$("#start").click(playGame);
-	$("#stop").click(function() { location.reload(); });
-	$("#blinker").click(placeBlinkerCells);
-	$("#toad").click(placeToadCells);
+  initialiseGame();
+  cells = table.find("td");
+  //placeRandomCells();
+  //playGame();
+  $("#randomize").click(placeRandomCells);
+  $("#start").click(playGame);
+  $("#stop").click(function() { location.reload(); });
+  $("#blinker").click(placeBlinkerCells);
+  $("#toad").click(placeToadCells);
 });
 
 function initialiseGame() {
-	var trHtml = [];
-	for (var y = 0; y < dimension; ++y) {
-	trHtml.push("<tr>");
-		for (var x = 0; x < dimension; ++x) {
-			trHtml.push("<td>&nbsp;</td>");
-		}
-		trHtml.push("</tr>");
-	}
-	trHtml = trHtml.join("");
-	table.append($(trHtml));
+  var trHtml = [];
+  for (var y = 0; y < dimension; ++y) {
+  trHtml.push("<tr>");
+    for (var x = 0; x < dimension; ++x) {
+      trHtml.push("<td>&nbsp;</td>");
+    }
+    trHtml.push("</tr>");
+  }
+  trHtml = trHtml.join("");
+  table.append($(trHtml));
 }
 
 function placeRandomCells() {
-	for (var y = 0; y < dimension; ++y) {
-		for (var x = 0; x < dimension; ++x) {
-			var cell = getCell(x,y);
-			if (Math.random() > chanceOfLiveCell) {
-				cell.addClass("alive");
-			} else {
-				cell.removeClass("alive");
-			}
-		}
-	}
+  for (var y = 0; y < dimension; ++y) {
+    for (var x = 0; x < dimension; ++x) {
+      var cell = getCell(x,y);
+      if (Math.random() > chanceOfLiveCell) {
+        cell.addClass("alive");
+      } else {
+        cell.removeClass("alive");
+      }
+    }
+  }
 }
 
 function placeBlinkerCells() {
@@ -67,58 +67,58 @@ function placeToadCells() {
 }
 
 function playGame() {
-	prepareNextGeneration();
-	renderNextGeneration();
-	setTimeout('playGame()', 200);
+  prepareNextGeneration();
+  renderNextGeneration();
+  setTimeout('playGame()', 200);
 }
 
 function prepareNextGeneration() {
-	for (var y = 0; y < dimension; ++y) {
-		for (var x = 0; x < dimension; ++x) {
-			var cell = getCell(x,y);
-			var neighbors = getLiveNeighborCount(x,y);
-			cell.attr("isalive", "false");
-			if (isCellAlive(x,y)) {
-				if (neighbors === 2 || neighbors === 3) {
-					cell.attr("isalive", "true");
-				}
-			} else if (neighbors === 3) {
-				cell.attr("isalive", "true");
-			}
-		}
-	}
+  for (var y = 0; y < dimension; ++y) {
+    for (var x = 0; x < dimension; ++x) {
+      var cell = getCell(x,y);
+      var neighbors = getLiveNeighborCount(x,y);
+      cell.attr("isalive", "false");
+      if (isCellAlive(x,y)) {
+        if (neighbors === 2 || neighbors === 3) {
+          cell.attr("isalive", "true");
+        }
+      } else if (neighbors === 3) {
+        cell.attr("isalive", "true");
+      }
+    }
+  }
 }
 
 function renderNextGeneration() {
-	cells.each(function () {
-		var cell = $(this);
-		cell.removeClass("alive");
+  cells.each(function () {
+    var cell = $(this);
+    cell.removeClass("alive");
       if (cell.attr("isalive") === "true") { cell.addClass("alive"); }
-		cell.removeAttr("isalive");
-	});
+    cell.removeAttr("isalive");
+  });
 }
 
 function getLiveNeighborCount(x,y) {
-	var count = 0;
-	if (isCellAlive(x-1, y-1)) {count++;}
-	if (isCellAlive(x, y-1)) {count++;}
-	if (isCellAlive(x+1, y-1)) {count++;}
-	if (isCellAlive(x-1, y)) {count++;}
-	if (isCellAlive(x+1, y)) {count++;}
-	if (isCellAlive(x-1, y+1)) {count++;}
-	if (isCellAlive(x, y+1)) {count++;}
-	if (isCellAlive(x+1, y+1)) {count++;}
-	return count;
+  var count = 0;
+  if (isCellAlive(x-1, y-1)) {count++;}
+  if (isCellAlive(x, y-1)) {count++;}
+  if (isCellAlive(x+1, y-1)) {count++;}
+  if (isCellAlive(x-1, y)) {count++;}
+  if (isCellAlive(x+1, y)) {count++;}
+  if (isCellAlive(x-1, y+1)) {count++;}
+  if (isCellAlive(x, y+1)) {count++;}
+  if (isCellAlive(x+1, y+1)) {count++;}
+  return count;
 }
 
 function isCellAlive(x,y) {
-	return getCell(x,y).attr("class") === "alive";
+  return getCell(x,y).attr("class") === "alive";
 }
 
 function getCell(x,y) {
-	if (x >= dimension) {x = 0;}
-	if (y >= dimension) {y = 0;}
-	if (x < 0) {x = dimension - 1;}
-	if (y < 0) {y = dimension - 1;}
-	return $(cells[y * dimension + x]);
+  if (x >= dimension) {x = 0;}
+  if (y >= dimension) {y = 0;}
+  if (x < 0) {x = dimension - 1;}
+  if (y < 0) {y = dimension - 1;}
+  return $(cells[y * dimension + x]);
 }
